7991613f6db16bcbeefdffdccc636b50796183a9
Garrick Suemith <garrick.suemith@gmail.com>
2021-04-16 06:29:04 -0400

    all tests passing

--- api/task/router.js
+++ api/task/router.js
@@ -4,6 +4,29 @@ const Tasks = require('./model')
 
 const router = express.Router()
 
+router.get('/', (req, res, next) => {
+  Tasks.find()
+  .then(tasks => {
+    res.status(200).json(trueOrFalse(tasks))
+  })
+  .catch(next)
+})
 
+router.post('/', (req, res, next) => {
+  Tasks.add(req.body)
+  .then(task => {
+    res.status(201).json(trueOrFalse(task)[0])
+  })
+  .catch(next)
+})
+
+const trueOrFalse = tasks => {
+  return tasks.map(proj => {
+    return {
+      ...proj,
+      task_completed: proj.task_completed ? true : false
+    }
+  })
+}
 
 module.exports = router
