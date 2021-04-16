79a3aa1c585ae4ac92fb250702f2567f1af4f3a0
Garrick Suemith <garrick.suemith@gmail.com>
2021-04-16 04:31:05 -0400

    installations and scaffolding

--- api/server.js
+++ api/server.js
@@ -1,1 +1,20 @@
 // build your server here and require it from index.js
+const express = require('express')
+const helmet = require('helmet')
+
+const projectRouter = require('./project/router')
+const resourceRouter = require('./resource/router')
+const taskRouter = require('./task/router')
+
+const server = express()
+
+//global middlewares
+server.use(helmet())
+server.use(helmet())
+
+//routers
+server.use('/api/projects', projectRouter)
+server.use('/api/resources', resourceRouter)
+server.use('/api/tasks', taskRouter)
+
+module.exports = server
